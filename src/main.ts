024f124e0d4b4cee7e3ccadfc8a6b09501cbe1fc
Federico Aloi <federico.aloi@gmail.com>
2022-12-08 18:33:18 -0400

    Mensaje de app ejecutándose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(4000);
+  new Logger('Main').log(`🚀 Aplicación ejecutándose en ${await app.getUrl()}`);
 }
 bootstrap();
