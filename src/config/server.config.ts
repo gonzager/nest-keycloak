82a8e81d77ec7ea25bb8037e3764fb815a12cf2c
Federico Aloi <federico.aloi@gmail.com>
2022-12-10 18:19:39 -0400

    Agregada configuración del entorno

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,12 +1,16 @@
 import { registerAs } from '@nestjs/config';
 
+type Env = 'development' | 'test' | 'production';
+
 export interface ServerConfig {
   port: number;
+  environment: Env;
 }
 
 export default registerAs(
   'server',
   (): ServerConfig => ({
     port: parseInt(process.env.SERVER_PORT, 10) || 4000,
+    environment: (process.env.NODE_ENV as Env) ?? 'development',
   }),
 );
