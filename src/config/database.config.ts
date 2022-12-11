9d1489335cae8e4f4f83c57785838a0c6e0e7c9d
Federico Aloi <federico.aloi@gmail.com>
2022-12-10 20:03:57 -0400

    Soporte para DATABASE_URL

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,11 +2,12 @@ import { registerAs } from '@nestjs/config';
 import typeOrmConfig from './typeOrm.config';
 
 export interface DatabaseConfig {
-  host: string;
-  port: number;
-  username: string;
-  password: string;
-  database: string;
+  url?: string;
+  host?: string;
+  port?: number;
+  username?: string;
+  password?: string;
+  database?: string;
 }
 
 export default registerAs('database', (): DatabaseConfig => typeOrmConfig);
--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -5,13 +5,21 @@ import serverConfig from './server.config';
 ConfigModule.forRoot({});
 const { environment } = serverConfig();
 
+const url = process.env.DATABASE_URL;
+
+const credentials = url
+  ? { url }
+  : {
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USERNAME || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'admin1234',
+      database: process.env.DATABASE_NAME || `contactos_${environment}`,
+    };
+
 const typeOrmConfig = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.DATABASE_USERNAME || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'admin1234',
-  database: process.env.DATABASE_NAME || `contactos_${environment}`,
+  ...credentials,
 };
 
 export default typeOrmConfig;
